0619a60ed5a7b29adfe9b21f6b253b5b1ced8d2b
ravindu422 <thiranjayaravindu@gmail.com>
2025-01-28 18:09:05 +0530

    Add server file

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import cors from 'cors'
+
+//App config
+const app = express();
+const port = process.env.PORT || 5000
+
+//middlewares
+app.use(express.json())
+app.use(cors())
+
+//api endpoints
+app.get('/',(req,res) => {
+    res.send("API Working")
+})
+
+app.listen(port, () => {
+    console.log(`Server is up and running on port : ${port}`);
+})
