34c7d0708d5acbdb4535568f328bfa9035bc7802
ravindu422 <thiranjayaravindu@gmail.com>
2025-01-28 22:02:11 +0530

    Made changes for import

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import cors from 'cors'
-import connectDB from './config/database';
+import connectDB from './config/database.js';
 
 //App config
 const app = express();
