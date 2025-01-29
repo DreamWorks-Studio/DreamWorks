5beed919a088af3d65f5b4507af6e8d5b3500a30
ravindu422 <thiranjayaravindu@gmail.com>
2025-01-29 18:28:23 +0530

    Remove some comments

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,17 +8,14 @@ import Footer from '../components/Footer';
 function App() {
   return (
     <div className="min-h-screen flex flex-col">
-      {/* Full-Width Navbar */}
       <Navbar className="w-full bg-black" />
-      
-      {/* Main Content Wrapper */}
+
       <main className="flex-grow px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
         <Routes>
-          {/* Add your routes here */}
+
         </Routes>
       </main>
 
-      {/* Full-Width Footer */}
       <Footer className="w-full bg-black" />
     </div>
   );
