665b00668a9bab9a4f7ad49213cfa2077fe7fa27
ravindu422 <thiranjayaravindu@gmail.com>
2025-01-30 18:59:02 +0530

    Modify footer

--- frontend/components/Footer.jsx
+++ frontend/components/Footer.jsx
@@ -1,60 +1,66 @@
-import React from "react";
-import { FaFacebook, FaInstagram, FaWhatsapp } from "react-icons/fa";
+import React from 'react'
+import { FaFacebook, FaInstagram, FaMapMarkerAlt, FaMobileAlt, FaWhatsapp } from 'react-icons/fa';
 
 const Footer = () => {
   return (
-    <footer className="bg-black px-4 md:px-16 lg:px-28 text-white">
-      <div className="max-w-7xl mx-auto border-t border-amber-600 py-8">
-        <div className="w-full grid grid-cols-1 md:grid-cols-3 gap-10 mb-8">
-          <div>
-            <img
-              src="/src/assets/web_logo.png"
-              alt="DreamWorks Studio Logo"
-              className="w-42 h-auto"
-            />
-          </div>
-          <div>
-            <h2 className="text-lg font-bold mb-4 ">Contact Us</h2>
-            <p className="text-gray-300">
-              <strong>Address:</strong> 
-              <br/>DreamWork Studio
-              <br/>Yampanwatta, 2nd Lane
-              <br/>Badulla
-              <br/>Sri Lanka
-              <br/><br/>
-              <strong>Hotline:</strong> +94721908494/ +94769655970
-            </p>
-          </div>
-          <div>
-            <h2 className="text-lg font-bold mb-4">Connect with Us</h2>
-            <ol className="space-y-4">
-              <li className="flex items-center space-x-2">
-                <FaFacebook className="text-white-500 text-xl" />
-                <a href="#" className="hover:underline text-gray-300">
-                  Facebook
-                </a>
-              </li>
-              <li className="flex items-center space-x-2">
-                <FaInstagram className="text-white-500 text-xl" />
-                <a href="#" className="hover:underline text-gray-300">
-                  Instagram
-                </a>
-              </li>
-              <li className="flex items-center space-x-2">
-                <FaWhatsapp className="text-white-500 text-xl" />
-                <a href="#" className="hover:underline text-gray-300">
-                  Whatsapp
-                </a>
-              </li>
-            </ol>
-          </div>
+    <div className="pt-4 px-4 md:px-20 lg:px-32 bg-black w-full overflow-hidden"id="Footer">
+        <div className="max-w-7xl mx-auto border-t border-gray-500 pt-8">
+            <div className='container mx-auto flex flex-col md:flex-row justify-between items-start ml-4 md:ml-14'> 
+                <div className='w-full md:w-1/4 mb-8 md:mb-0'>
+                    <img src="src/assets/web_logo.png" alt="" className='w-52'/>
+                </div>
+                <div className='w-full md:w-3/4 flex flex-row justify-center'>
+                    <div className='w-full md:w-1/3 mb-8 md:mb-0'>
+                        <h3 className='text-gray-400 text-lg font-bold mb-4 underline underline-offset-8 decoration-amber-600'>Contact Us</h3>
+                        <div className='text-gray-400 text-sm ml-4'>
+                            <div className='flex items-center space-x-2.5 mt-4 text-sm'>
+                                <FaMapMarkerAlt className='mb-1 mt-1 mr-1 text-amber-600'/><strong>Address:</strong>
+                            </div>
+                            <p className='ml-4'>DreamWork Studio <br/>
+                                   Yampanwatta, <br/>2nd Lane, Badulla, <br/>
+                                   Sri Lanka
+                            </p>
+                            <div className='flex items-center space-x-2.5 mt-4 text-sm'>
+                                <FaMobileAlt className='mb-1 mt-1 mr-1 text-amber-600'/><strong>Mobile:</strong>
+                            </div>
+                                <p className='ml-4'>+94-72-190-8494/<br/>
+                                +94-76-965-5970</p>
+                            
+                        </div>
+                    </div>
+                    <div className='w-full md:w-1/3 mb-8 md:mb-0 md:pl-4'>
+                        <h3 className='text-gray-400 text-lg font-bold mb-4 underline underline-offset-8 decoration-amber-600'>Quick Links</h3>
+                        <ul className='list-[square] flex flex-col gap-2.5 text-gray-400 marker:text-amber-600 ml-6'>
+                            <li><a href="#Header" className='hover:text-amber-600'>Home</a></li>
+                            <li><a href="#About" className='hover:text-amber-600'>About</a></li>
+                            <li><a href="#" className='hover:text-amber-600'>Gallery</a></li>
+                        </ul>
+                    </div>
+                    <div className='w-full md:w-1/3'>
+                        <h3 className='text-gray-400 text-lg font-bold mb-4 underline underline-offset-8 decoration-amber-600'>Connect with Us</h3>
+                        <ul className='flex flex-col gap-4 mb-4 max-w-80'>
+                            <li className='flex items-center space-x-2.5'>
+                                <FaFacebook className='text-gray-400 text-2xl' />
+                                <a href="#" className='text-gray-400 hover:text-amber-600'>Facebook</a>
+                            </li>
+                            <li className='flex items-center space-x-2.5'>
+                                <FaInstagram className='text-gray-400 text-2xl' />
+                                <a href="#" className='text-gray-400 hover:text-amber-600'>Instagram</a>
+                            </li>
+                            <li className='flex items-center space-x-2.5'>
+                                <FaWhatsapp className='text-gray-400 text-2xl' />
+                                <a href="#" className='text-gray-400 hover:text-amber-600'>WhatsApp</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div className='border-t border-gray-500 py-3 mt-10 text-center text-sm text-gray-400'>
+                Copyrights <span className='text-amber-600'>© </span>DreamWorks Photography Studio.2025 | All rights reserved.
+            </div>
         </div>
-        <div className="border-amber-600 border-t w-full py-3 text-gray-300 text-center">
-          <p>© 2025 DreamWorks Photography Studio. All rights reserved.</p>
-        </div>
-      </div>
-    </footer>
+    </div>
   );
-};
+}
 
-export default Footer;
+export default Footer
