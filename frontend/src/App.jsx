de39f6a9cbc67fa772ed5b30673f57db0fd428ce
ravindu422 <thiranjayaravindu@gmail.com>
2025-01-30 19:11:45 +0530

    Update index.css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import ScrollUp from './components/ScrollUp'
-import About from '../pages/About'
-import Contact from '../pages/Contact'
+import Header from '/components/Header'
+import Footer from '/components/Footer'
+import ScrollUp from '/components/ScrollUp'
+import About from '/pages/About'
+import Contact from '/pages/Contact'
 
 const App = () => {
   return (
