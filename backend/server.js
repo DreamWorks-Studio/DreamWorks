67c2900ff63c203e3c118697c0a1bb540acb1404
Sadumina Rathnayaka <IT23394124@my.sliit.lk>
2025-02-03 21:00:10 +0530

    create middleware and utils for handle the errors

--- backend/server.js
+++ backend/server.js
@@ -27,3 +27,16 @@ app.listen(port, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth" , authRouter);
+
+//setting up the middleware 
+
+app.use((err, req , res ,next)=> {
+
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
