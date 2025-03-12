5441a3575e49e52ec34d60e8f7e208946a49bcf7
ravindu422 <thiranjayaravindu@gmail.com>
2025-03-12 14:08:45 +0530

    modify app file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,24 @@
 import React from 'react'
+
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Header from './components/Header'
-import About from './pages/About'
-import Contact from './pages/Contact'
-import Footer from './components/Footer'
-import ScrollUp from './components/ScrollUp'
-import Gallery from './pages/Gallery'
-import Navbar from './components/Navbar'
-import SignUp from './pages/SignUp'
+import Header from '../components/Header'
+import Contact from '../pages/Contact'
+import ScrollUp from '../components/ScrollUp'
+import Footer from '../components/Footer'
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div className='w-full overflow-hidden'>
-        <Routes>
-          <Route path="/" element={<Header />} />
-          <Route path="/gallery" element={<Gallery />} />
-          <Route path='/contact' element={<Contact />} />
-          <Route path='/signup' element={<SignUp/>} />
-        </Routes>
-        <ScrollUp />
-        <Footer />
-      </div>
+    <div className='w-full overflow-hidden'>
+    <Routes>
+      <Route path='/' element={<Header/>}/>
+      <Route path='/contact' element={<Contact />}/>
+    </Routes>
+    <ScrollUp/>
+    <Footer/>
+    </div>
     </BrowserRouter>
+    
   )
 }
 
