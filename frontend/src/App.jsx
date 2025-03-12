abe872de221ef2979f41e6e73f15227178044bf1
ravindu422 <thiranjayaravindu@gmail.com>
2025-03-12 12:27:11 +0530

    Changes for App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,28 @@
 import React from 'react'
-import Header from '/components/Header'
-import Footer from '/components/Footer'
-import ScrollUp from '/components/ScrollUp'
-import About from '/pages/About'
-import Contact from '/pages/Contact'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Header from './components/Header'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Footer from './components/Footer'
+import ScrollUp from './components/ScrollUp'
+import Gallery from './pages/Gallery'
+import Navbar from './components/Navbar'
+import SignUp from './pages/SignUp'
 
 const App = () => {
   return (
-    <div className='w-full overflow-hidden'>
-      <Header/>
-      <About/>
-      <Contact/>
-      <Footer/>
-      <ScrollUp/>
-    </div>
+    <BrowserRouter>
+      <div className='w-full overflow-hidden'>
+        <Routes>
+          <Route path="/" element={<Header />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/signup' element={<SignUp/>} />
+        </Routes>
+        <ScrollUp />
+        <Footer />
+      </div>
+    </BrowserRouter>
   )
 }
 
