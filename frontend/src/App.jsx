93dba03753f97ede4ae8216d271d375cb861b597
ravindu422 <thiranjayaravindu@gmail.com>
2025-03-15 19:18:43 +0530

    Update App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,10 @@ import Header from '../components/Header'
 import Contact from '../pages/Contact'
 import ScrollUp from '../components/ScrollUp'
 import Footer from '../components/Footer'
+import PaymentSummary from '../pages/PaymentSummary'
+import PaymentGateway from '../pages/PaymentGateway'
+import AdminDashboard from '../pages/AdminDashboard'
+import { ToastContainer } from 'react-toastify'
 
 const App = () => {
   return (
@@ -13,9 +17,12 @@ const App = () => {
     <Routes>
       <Route path='/' element={<Header/>}/>
       <Route path='/contact' element={<Contact />}/>
+      <Route path='/payment' element={<PaymentSummary/>}/>
+      <Route path='/gateway' element={<PaymentGateway/>}/>
+      <Route path='/dashboard' element={<AdminDashboard/>}/>
     </Routes>
     <ScrollUp/>
-    <Footer/>
+    <ToastContainer/>
     </div>
     </BrowserRouter>
     
