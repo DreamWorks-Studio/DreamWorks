526670eb90f63da8a4fc38bf7441cc49c836fa57
ravindu422 <thiranjayaravindu@gmail.com>
2025-03-15 19:33:52 +0530

    add route for get payment details

--- backend/routes/payment.route.js
+++ backend/routes/payment.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
 const router = express.Router();
-import { test, createPayment, processPayment } from '../controller/payment.controller.js';
+import { test, createPayment, processPayment, getPayments } from '../controller/payment.controller.js';
 
 router.get('/test', test);
 
 router.post('/', createPayment);
 router.post('/payment', processPayment);
+router.get('/getPayment', getPayments);
 
 export default router;
