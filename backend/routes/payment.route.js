e1d1ad38e47ebc465737618cdf1afa793aa35c5f
ravindu422 <thiranjayaravindu@gmail.com>
2025-03-15 19:25:07 +0530

    add route for payment process

--- backend/routes/payment.route.js
+++ backend/routes/payment.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
 const router = express.Router();
-import { test, createPayment } from '../controller/payment.controller.js';
+import { test, createPayment, processPayment } from '../controller/payment.controller.js';
 
 router.get('/test', test);
 
 router.post('/', createPayment);
+router.post('/payment', processPayment);
 
 export default router;
