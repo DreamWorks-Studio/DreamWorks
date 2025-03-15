2d0fd52233a5e6d27eaf78ba044e0f6aba1fa7a5
Sadumina Rathnayaka <IT23394124@my.sliit.lk>
2025-03-15 13:35:32 +0530

    bug fix in profile

--- frontend/pages/UserProfile.jsx
+++ frontend/pages/UserProfile.jsx
@@ -3,14 +3,8 @@ import Navbar from '../components/Navbar'
 
 const UserProfile = () => {
   return (
-    <><div>
-
-           <h1>Profile</h1>
-
-
-      </div><Navbar /></>
+    <><div>UserProfile</div><Navbar></Navbar></>
   )
- 
 }
 
 export default UserProfile
