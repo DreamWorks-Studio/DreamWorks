99b6664db6626622c92f5160bc7e2260550ba115
ravindu422 <thiranjayaravindu@gmail.com>
2025-03-15 17:51:07 +0530

    created controller for finance

---
+++ backend/controller/payment.controller.js
@@ -0,0 +1,56 @@
+import Payment from "../model/payment.model.js";
+
+export const test = (req,res) => {
+    res.json({
+
+        message : 'API  route is Working !!',
+    });
+};
+
+export const createPayment = async (req, res) => {
+    
+    try {
+        console.log('Payment endpoint hit');
+        console.log('Headers:', req.headers);
+        console.log('Received request body:', req.body);
+
+        const { bookingId, amountPaid, paymentMethod } = req.body;
+
+        
+        if (!bookingId || !amountPaid || !paymentMethod) {
+            return res.status(400).json({ message: 'Missing required fields' });
+        }
+
+        if (paymentMethod === 'card') {
+            if (!paymentType || !['full', 'partial'].includes(paymentType)) {
+              return res.status(400).json({ message: 'Valid payment type (full/partial) is required for card payments' });
+            }
+
+            if(paymentType === 'partial' && amountPaid < 3000) {
+                return res.status(400).json({ message: 'Partial payments must be at least 3000' });
+            }
+        }
+          
+        let paymentStatus;
+        if (paymentMethod === 'cash') {
+            paymentStatus = 'pending';
+        } else if (paymentMethod === 'card') {
+            paymentStatus = paymentType === 'partial' ? 'partial' : 'completed';
+        }
+
+        const payment = new Payment({
+            bookingId,
+            amountPaid,
+            paymentMethod,
+            paymentStatus,
+            paymentType: paymentMethod === 'card' ? paymentType : null
+          });
+
+        await payment.save();
+        res.status(201).json({ message: 'Payment recorded successfully', payment });
+
+    } catch (error) {
+        console.error('Error processing payment:', error);
+        res.status(500).json({ message: 'Failed to process payment', error: error.message });
+    }
+};
