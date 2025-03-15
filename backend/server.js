70dcb6a82a866deda15e09c71c157708f41aa875
ravindu422 <thiranjayaravindu@gmail.com>
2025-03-15 17:55:05 +0530

    add routes path for payment

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import connectDB from './config/database.js';
+import paymentRouter from './routes/payment.route.js';
 
 dotenv.config();
 
@@ -11,13 +12,17 @@ const port = process.env.PORT || 5003
 connectDB()
 
 //middlewares
-app.use(express.json())
 app.use(cors())
+app.use(express.json())
+app.use((req, res, next) => {
+    console.log('Request body before route:', req.body);  // Debug line
+    next();
+});
+
+
 
 //api endpoints
-app.get('/',(req,res) => {
-    res.send("API Working")
-})
+app.use('/api/payments', paymentRouter);
 
 app.listen(port, () => {
     console.log(`Server is up and running on port : ${port}`);
