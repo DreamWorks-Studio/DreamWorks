2cf8dfdcee9899acf7e0142d3b055eeef521a74f
ravindu422 <thiranjayaravindu@gmail.com>
2025-03-17 10:37:52 +0530

    remove processPayment route

--- backend/routes/payment.route.js
+++ backend/routes/payment.route.js
@@ -1,11 +1,13 @@
 import express from 'express';
 const router = express.Router();
-import { test, createPayment, processPayment, getPayments } from '../controller/payment.controller.js';
+import { test, createPayment, getPayments, generateInvoice, onSubmit } from '../controller/payment.controller.js';
 
 router.get('/test', test);
 
 router.post('/', createPayment);
-router.post('/payment', processPayment);
+//router.post('/payment', processPayment);
 router.get('/getPayment', getPayments);
+router.post('/generate-invoice', generateInvoice);
+router.post('/saveCard', onSubmit);
 
 export default router;
