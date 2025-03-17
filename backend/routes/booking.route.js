29e493d389c201c731bc1546f75e4391e931240a
OhaseeLanka <ohasilanka6@gmail.com>
2025-03-17 16:40:23 +0530

    modify booking route

--- backend/routes/booking.route.js
+++ backend/routes/booking.route.js
@@ -1,10 +1,13 @@
 import express from 'express';
 const router = express.Router();
 
-import { test } from '../controller/booking.controller.js';
+import { test, createBooking, getBooking, getAllBookings,updateBooking } from '../controller/booking.controller.js';
+
 
 router.get('/test',test);
-
-
+router.post('/add',createBooking);
+router.get('/display-summary', getAllBookings);
+router.get('/display-summary/:id',getBooking);
+router.put('/update-booking/:id',updateBooking);
 
 export default router;
