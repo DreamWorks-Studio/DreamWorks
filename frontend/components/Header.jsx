bf07d44973584bbe37b266f5b78e7e001adee3c6
Sadumina Rathnayaka <IT23394124@my.sliit.lk>
2025-03-24 14:56:46 +0530

    add footer

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './Navbar'
 import Carousel from './Carousel'
 import About from '../pages/About'
+import Footer from '../components/Footer'
 import PrivateRoute from './PrivateRoute'
 
 const Header = () => {
@@ -10,6 +11,7 @@ const Header = () => {
         <Navbar/>
         <Carousel/>
         <About/>
+        <Footer/>
         <PrivateRoute/>
     </div>
   )
