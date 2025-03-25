31685b070da103a2648ed408f3d788d17648d7e7
Ijasahd123 <ijasahd111971@gmail.com>
2025-03-25 23:51:56 +0530

    modify routes for packages

--- backend/routes/auth.route.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { promo } from '../controllers/auth.controller.js';
-
-const router = express.Router();
-
-router.post('/promo', promo);
-
-export default router;
--- backend/routes/package.route.js
+++ backend/routes/package.route.js
@@ -1,9 +1,15 @@
 import express from 'express';
 import { test } from '../controllers/package.controller.js';
-import { updateUser } from '../controllers/package.controller.js';
+import { promo, updateUser, getPackages, updatePackage, deletePackage, getPackage } from '../controllers/package.controller.js';
 const router = express.Router();
 
 router.get('/test', test )
+router.post('/addPackage', promo);
 router.put('/update:packageId', updateUser);
+router.get('/viewPackages', getPackages);
+router.put('/updatePackage/:id', updatePackage);
+router.delete('/deletePackage/:id', deletePackage);
+router.get('/getPackage/:id', getPackage);
+
 
 export default router;
