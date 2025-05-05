9b4593404bda3ef3e1914fba02db2857037c70ba
ravindu422 <thiranjayaravindu@gmail.com>
2025-05-05 19:39:30 +0530

    update model for ratings

--- backend/model/portfolio.model.js
+++ backend/model/portfolio.model.js
@@ -6,9 +6,23 @@ const portfolioSchema = new mongoose.Schema(
     //publicId: { type: String, required: true }, // Cloudinary Image ID for deletion
     category: { type: String, required: true },
     description: { type: String },
+    ratings: [{
+      userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
+      rating: { type: Number, required: true, min: 1, max: 5}
+    }]
   },
   { timestamps: true }
 );
+
+portfolioSchema.virtual('averageRating').get(function() {
+  if (!this.ratings || this.ratings.length === 0) return 0;
+  
+  const sum = this.ratings.reduce((total, current) => total + current.rating, 0);
+  return (sum / this.ratings.length).toFixed(1);
+});
+
+portfolioSchema.set('toJSON', { virtuals: true });
+portfolioSchema.set('toObject', { virtuals: true });
 
 const Portfolio = mongoose.model("Portfolio", portfolioSchema);
 export default Portfolio;
