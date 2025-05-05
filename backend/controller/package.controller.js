6068691080ce65a7b3f4d20565c62a3e0f25f720
ravindu422 <thiranjayaravindu@gmail.com>
2025-05-05 19:09:11 +0530

    add customizations for functions

--- backend/controller/package.controller.js
+++ backend/controller/package.controller.js
@@ -6,27 +6,32 @@ export const test = (req,res)=> {
 };
 
 export const promo = async (req, res) => {
-    try {
-        const { packagename, packageDetails, packagePrice, packagevalidity } = req.body;
-
-        if (!packagename || !packageDetails || !packagePrice || !packagevalidity) {
-            return res.status(400).json({ error: "All fields are required." });
-        }
-
-        const newPackage = new Package({
-            packagename,
-            packageDetails,
-            packagePrice,
-            packagevalidity,
-        });
-
-        await newPackage.save();
-        res.status(201).json({ message: "Adding package is successful" });
-
-    } catch (error) {
-        console.error("Error adding package:", error.message);
-        res.status(500).json({ error: "Internal Server Error. Please try again." });
-    }
+  try {
+      const { 
+          packagename, 
+          packageDetails, 
+          packagePrice, 
+          packagevalidity, 
+          packageType, 
+          includedCustomizations 
+      } = req.body;
+      if (!packagename || !packageDetails || !packagePrice || !packagevalidity) {
+          return res.status(400).json({ error: "All fields are required." });
+      }
+      const newPackage = new Package({
+          packagename,
+          packageDetails,
+          packagePrice,
+          packagevalidity,
+          packageType: packageType || "Event",
+          includedCustomizations: includedCustomizations || []
+      });
+      await newPackage.save();
+      res.status(201).json({ message: "Adding package is successful" });
+  } catch (error) {
+      console.error("Error adding package:", error.message);
+      res.status(500).json({ error: "Internal Server Error. Please try again." });
+  }
 };
 
 
@@ -90,7 +95,9 @@ export const updatePackage = async (req, res) => {
         packagename, 
         packageDetails, 
         packagePrice, 
-        packagevalidity 
+        packagevalidity,
+        packageType,
+        includedCustomizations 
       } = req.body;
   
       // Validate MongoDB ID
@@ -125,7 +132,9 @@ export const updatePackage = async (req, res) => {
           packagename,
           packageDetails,
           packagePrice,
-          packagevalidity
+          packagevalidity,
+          packageType: packageType || "Others",
+          includedCustomizations: includedCustomizations || []
         }, 
         { 
           new: true,  // Return the updated document
