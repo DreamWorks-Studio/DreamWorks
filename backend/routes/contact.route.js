37494a9b230c40320557fe6105a484940e4c1722
ravindu422 <thiranjayaravindu@gmail.com>
2025-05-06 15:43:08 +0530

    add routes for view and delete

--- backend/routes/contact.route.js
+++ backend/routes/contact.route.js
@@ -1,9 +1,12 @@
 import express from 'express';
 // routes/contact.route.js
-import { createContactMessage } from '../controller/contact.controller.js';
+import { createContactMessage, getAllContactMessages, deleteContactMessage } from '../controller/contact.controller.js';
 
 const router = express.Router();
 
 router.post('/contact', createContactMessage);
+router.get('/contact/messages', getAllContactMessages);
+router.delete('/contact/messages/:id', deleteContactMessage);
+
 
 export default router;
